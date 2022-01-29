e6088416ccb87309e298338c4299d0feb4408cc5
jugalkpatel <ptjimmy99@gmail.com>
2022-01-29 23:25:00 +0530

    refactor: change totalTime to time and added transform in history model

--- models/history.model.js
+++ models/history.model.js
@@ -2,24 +2,39 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const historySchema = Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
+const historySchema = Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    quiz: {
+      type: Schema.Types.ObjectId,
+      ref: "Quiz",
+    },
+    score: {
+      type: Number,
+      required: true,
+    },
+    time: {
+      type: Number,
+      required: true,
+    },
   },
-  quiz: {
-    type: Schema.Types.ObjectId,
-    ref: "Quiz",
-  },
-  score: {
-    type: Number,
-    required: true,
-  },
-  totalTime: {
-    type: Number,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        delete ret.createdAt;
+        delete ret.updatedAt;
+        return ret;
+      },
+    },
+  }
+);
 
 const History = model("History", historySchema);
 
